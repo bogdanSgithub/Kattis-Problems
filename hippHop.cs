85061abdcef123dc42b9ffda8e2531c9f98236c3
Bogdan Feher <bogdan.feher@gmail.com>
2024-10-31 07:04:13 -0400

    added cracking RSA and hipp hop solutions

---
+++ hippHop.cs
@@ -0,0 +1,16 @@
+using System;
+namespace HippHipp
+{   
+    class Program
+    {
+        static void Main() 
+        {
+            const int N = 20;
+            const string s = "Hipp hipp hurra!";
+            for (int i=0; i<N; i++) 
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
