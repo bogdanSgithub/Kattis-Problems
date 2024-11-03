a2f32e7a430c9f5053f6ee98bec38885cec7ecaf
Bogdan Feher <bogdan.feher@gmail.com>
2024-11-02 21:40:22 -0400

    refactored zigZag solution

--- zigZag.cs
+++ zigZag.cs
@@ -10,51 +10,41 @@ namespace Zigzag
             int n = int.Parse(Console.ReadLine());
             const int AsciiA = 97;
             const int Distance = 25;
-            int size = Convert.ToInt32(Math.Ceiling((decimal)n / Distance)) + 1;
+            int size = Convert.ToInt32(Math.Ceiling((decimal) n / Distance)) + 1;
 
             char[] answer = new char[size];
-            answer[0] = 'a';
-            int asciiLetter; 
-
-            if (size == 2)
+            // theres only 2 variables in our answer, the second letter and the last letter. everything else is a or z.
+            for (int i = 0; i < size; i++)
             {
-                answer[1] = (char)(AsciiA + n);
-            }
-            else if (size == 3)
-            {   
-                asciiLetter = Convert.ToInt32(Math.Ceiling((decimal)n / 2));
-                answer[1] = (char)(AsciiA + asciiLetter);
-                if (n % 2 == 0)
-                    answer[2] = 'a';
+                if (i % 2 == 0)
+                    answer[i] = 'a';
                 else
-                    answer[2] = 'b';
+                    answer[i] = 'z';
             }
-            else
+            // this takes care of if size == 2 which is when n < 26
+            int secondLetter = n;
+
+            if (size == 3)
+            {   
+                secondLetter = Convert.ToInt32(Math.Ceiling((decimal) n / 2));
+                if (n % 2 != 0)
+                    answer[size-1] = 'b';
+            }
+            else if (size > 3)
             {
                 int sum = (size - 3) * Distance;
-                asciiLetter = Convert.ToInt32(Math.Ceiling((decimal)(n - sum) / 2));
-
-                for (int i = 1; i < size; i++)
-                {
-                    if (i % 2 == 0)
-                        answer[i] = 'a';
-                    else
-                        answer[i] = 'z';
-                }
+                secondLetter = Convert.ToInt32(Math.Ceiling((decimal)(n - sum) / 2));
 
                 if (answer[size - 1] == 'a' && n % 2 == 1)
                     answer[size - 1] = 'b';
 
                 else if (answer[size - 1] == 'z' && n % 2 == 0)
                     answer[size - 1] = 'y';
-
-                answer[1] = (char)(AsciiA + asciiLetter);
             }
 
+            answer[1] = (char)(AsciiA + secondLetter);
             foreach (char c in answer)
-            {
                 Console.Write(c);
-            }
         }
     }
 }
